7c2c7c0c907f3c84d17e62dcf3fa8ee4a8d6067b
Caresle <carabesle@gmail.com>
2021-09-04 14:55:16 -0600

    Update mongoose to 6.0.4

--- src/db.js
+++ src/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI
-mongoose.connect(MONGO_URI, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true
-})
+mongoose.connect(MONGO_URI)
 
 const db = mongoose.connection
 
